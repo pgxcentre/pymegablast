928129705def5f560eec31985deb6a0b0e8e6cb9
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2013-10-22 13:28:29 -0400

    Added numpy as a requirement, because of biopython

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name="wrapper_megablast",
       url="http://www.statgen.org",
       license="GPL",
       scripts=[os.path.join("scripts", "py_megablast"),],
-      install_requires=["biopython >=1.62",],
+      install_requires=["biopython >=1.62", "numpy >= 1.7.1"],
 ##       packages=["pyGenClean", "PlinkUtils", "pyGenClean.Misc"] +
 ##                ["pyGenClean.Step{}".format(i) for i in xrange(1, 13)],
       classifiers=['Operating System :: Linux',
